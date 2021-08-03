867ca3a4549c178973e23c1795d1f2c6d31f2956
git-leon <xleonhunter@gmail.com>
2021-08-03 15:45:27 -0400

    added  to MainApplicaiton

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -1,10 +1,12 @@
 package com.github.zipcodewilmington;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by leon on 2/5/18.
  */
+@SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
