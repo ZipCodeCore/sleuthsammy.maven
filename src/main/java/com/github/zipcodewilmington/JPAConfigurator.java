f0c2f4310d69e591ae7c01b1849782f86afec23b
git-leon <xleonhunter@gmail.com>
2021-08-03 16:03:39 -0400

    added MySQL8Dialect to 'application.properties'

---
+++ src/main/java/com/github/zipcodewilmington/JPAConfigurator.java
@@ -0,0 +1,65 @@
+package com.github.zipcodewilmington;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 8/3/21 3:57 PM
+ */
+public class JPAConfigurator {
+    public JPAConfigurator() {
+        try {// Attempt to register JDBC Driver
+            DriverManager.registerDriver(Driver.class.newInstance());
+        } catch (InstantiationException | IllegalAccessException | SQLException e1) {
+            throw new RuntimeException(e1);
+        }
+    }
+
+    public void init() {
+        Connection mysqlDbConnection = getConnection("mysql");
+        executeStatement(mysqlDbConnection, "DROP DATABASE IF EXISTS testdb;");
+        executeStatement(mysqlDbConnection, "CREATE DATABASE IF NOT EXISTS testdb;");
+    }
+
+    void executeStatement(Connection connection, String sqlStatement) {
+        try {
+            Statement statement = getScrollableStatement(connection);
+            statement.execute(sqlStatement);
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+
+    Statement getScrollableStatement(Connection connection) {
+        int resultSetType = ResultSet.TYPE_SCROLL_INSENSITIVE;
+        int resultSetConcurrency = ResultSet.CONCUR_READ_ONLY;
+        try { // scrollable statements can be iterated more than once without closing
+            return connection.createStatement(resultSetType, resultSetConcurrency);
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+
+    Connection getConnection(String dbVendor) {
+        String username = "root";
+        String password = "";
+        String url = new StringBuilder()
+                .append("jdbc:")
+                .append(dbVendor)
+                .append("://127.0.0.1/")
+                .append("?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC")
+                .toString();
+        try {
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new Error(e);
+        }
+    }
+}
--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
+        new JPAConfigurator().init();
         SpringApplication.run(MainApplication.class, args);
     }
 }
