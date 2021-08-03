850a3a2714f0712b6d17fb5296903dedd3a110f7
git-leon <xleonhunter@gmail.com>
2021-08-03 15:43:48 -0400

    refactored mainapplication

---
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -0,0 +1,12 @@
+package com.github.zipcodewilmington;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by leon on 2/5/18.
+ */
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
